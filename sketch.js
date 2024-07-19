cebd9fed779279136a459ab0387f5bbca4e8618f
Diya-1845 <diya.shah1845@gmail.com>
2024-07-19 10:18:21 +0530

    color strokeweight etc

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   createCanvas(1020, 1020);
 
-  background(76, 107, 158);
+  background(128, 66, 153);
   strokeWeight (18);
-  stroke ('black');
+  stroke ('white');
   frameRate(10);
   
 }
